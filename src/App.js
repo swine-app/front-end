d95d758a718906948afb53f6c22836f9afe500e9
Jonathan Montgomery <jonathan.montgo@gmail.com>
2022-09-11 23:31:29 -0500

    Remove styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import UserForm from './UserForm';
 // import Login from './Login';
 
@@ -7,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <UserForm/>
       </header>
     </div>
